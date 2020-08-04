8614a86b45368506617109187b81ed37a227173e
Chentao Xu <55690325+Kevinxct@users.noreply.github.com>
2020-08-04 23:58:47 +0800

    Update transformer.py

--- pyterrier/transformer.py
+++ pyterrier/transformer.py
@@ -208,6 +208,7 @@ class TransformerBase(object):
     def gridsearchCV(self, topics, qrels, param_map, metric='ndcg', **kwargs):
       from sklearn.model_selection import KFold
       import numpy as np
+      import pandas as pd
       KF=KFold(n_splits=kwargs['num_folds']) #n_splits can be tested and changed
       #all_score = []
       all_split_scores = pd.DataFrame({"qid":qrels['qid'].drop_duplicates()})
