7b032c14e4c35754f803960c398d519391d50161
Chentao Xu <55690325+Kevinxct@users.noreply.github.com>
2020-08-15 20:55:49 +0800

    Update transformer.py

--- pyterrier/transformer.py
+++ pyterrier/transformer.py
@@ -366,21 +366,17 @@ class NAryTransformerBase(TransformerBase,Operation):
         return len(self.models)
    
     def get_transformer(self,name):
-        n = 0
-        for m in self.models:
-          if not hasattr(m,"id"):
-            m.get_transformer(name)
-          #   m.models = list( map(lambda x : get_transformer(x), m) )
-          #   for i in m.models:
-          #     if id == i.id:
-          #       n += 1
-          #       return i
-          else:
-            if name == m.id:
-              n += 1
+      if hasattr(self,"id"):
+        if self.id == name:
+            return self
+      else:
+          for m in self.models:
+            if hasattr(m,“id”) and name == m.id:
               return m
-        if n == 0:
-          raise ValueError('No match object in pipeline')
+            rtr = m.get_transformer(name)
+            if rtr is not None:
+              return rtr
+      return None
 
 class SetUnionTransformer(BinaryTransformerBase):
     name = "Union"
